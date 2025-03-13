04daedf965cacfcc2345e039b06a6c2e4532dbe7
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-13 11:23:25 +0000

    Working on each meal

--- components/meals/meals-grid.tsx
+++ components/meals/meals-grid.tsx
@@ -1,3 +1,13 @@
-function MealsGrid() {}
+import styles from './meals-grid.module.css';
+
+function MealsGrid({ meals }) {
+  return (
+    <ul className={styles.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>{}</li>
+      ))}
+    </ul>
+  );
+}
 
 export default MealsGrid;
